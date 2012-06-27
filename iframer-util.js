3a61f6a645bdc2801c83fe05bdd42bcf9522de5a
peterhal@google.com <peterhal@google.com@43f60d33-9538-8bfc-46d6-4696f8465a1f>
2012-06-27 20:02:09 +0000

    Iframer working.

---
+++ iframer-util.js
@@ -0,0 +1,54 @@
+/**
+ * Extracts url parameters of the containing window/iframe into an object.
+ * @param {string} url The url to extract the parameters from.
+ * @return {!Object} An object containing the paarameters as name/value pairs.
+ */
+function getUrlParams(url) {
+  var result = {};
+  var paramRegex = /([^&=]+)=?([^&]*)/g;
+  var decode = function(value) {
+    var addRegex = /\+/g;
+    return decodeURIComponent(value.replace(addRegex, " "));
+  };
+  var startIndex =  url.indexOf('?') + 1;
+  if (startIndex > 0) {
+    var allParams = url.substring(url.indexOf('?') + 1);
+    while (match = paramRegex.exec(allParams)) {
+      result[decode(match[1])] = decode(match[2]);
+    }
+  }
+  return result;
+}
+
+/**
+ * Encodes an object as an url parameter string.
+ * @param {!Object} params An object containing the parameters to encode.
+ * @return {string} The encoded parameters. Includes leading '?'.
+ */
+function encodeUrlParams(params) {
+  var result = '';
+  for (var key in params) {
+    result += (result.length === 0) ? '?' : '&';
+    result += key + '=' + encodeURIComponent(params[key]);
+  }
+  return result;
+}
+
+
+/**
+ * Writes a tag to the document.
+ * @param {string} tag The tag to write.
+ */
+function writeTag(tag) {
+  document.write(tag);
+}
+
+
+/**
+ * Loads a javascript script by url.
+ * @param {string} url The url of the string to load.
+ */
+function loadScript(url) {
+  writeTag('<script type="text/javascript" src="' + url +
+      '"><' + '/script>'); // Split script tag so it isn’t used as end of tag.
+}
